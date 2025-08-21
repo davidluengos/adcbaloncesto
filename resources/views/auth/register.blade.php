8002f2cd8da885cc2c0adc640fc4c3ff43f6a9cd
davidluengos <davidluengosanchez@hotmail.com>
2025-08-21 18:31:38 +0200

    restringir url del register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,10 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <div>
-                        <p>No están permitidos nuevos usuarios.</p>
-                    </div>
-                    <!-- <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="row mb-3">
@@ -71,7 +68,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form> -->
+                    </form>
                 </div>
             </div>
         </div>
