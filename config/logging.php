ce9650e00ee34905378e07f24fe49f29d6dca6fc
davidluengos <davidluengosanchez@gmail.com>
2025-11-04 18:55:18 +0100

    añadir channel para mostrar logs de inserción de resultados en un archivo aparte

--- config/logging.php
+++ config/logging.php
@@ -113,6 +113,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'resultados' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/resultados.log'),
+            'level' => 'info',
+        ],
     ],
 
 ];
